035c1b9f157adf67f49a7aae7641e71328655043
Enzo Perego <enzorp@al.insper.edu.br>
2025-09-17 11:05:15 -0300

    start

---
+++ src/main/java/com/example/pi_projectsoft/PiProjectsoftApplication.java
@@ -0,0 +1,13 @@
+package com.example.pi_projectsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiProjectsoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiProjectsoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pi_projectsoft/PiProjectsoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pi_projectsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiProjectsoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
